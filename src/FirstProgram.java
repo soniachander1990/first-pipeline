e66422b1ea10e60664fb78b990b868adbe4553bb
sonia chander <soniachander7294@gmail.com>
2021-03-23 15:23:45 -0400

    First commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        int a=10;
+        int b=13;
+        int sum=a+b;
+
+        System.out.println("sum of a and b is:" +sum);
+    }
+}
